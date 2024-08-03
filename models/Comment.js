31d4f2033606be7b5b479268f3b5279d49441c45
Innovative-J <juan.cia05678@gmail.com>
2024-08-02 19:05:16 -0500

    created schema and completed comment model and added DS store to git ignore

---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes, INTEGER } = require("sequelize");
+const sequelize = require("../config/connections");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes,
+      INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Comment;
